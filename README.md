a78ee4c529be0c385a83ff375e679e5b607ef7e8
SAISHYAMR2002 <saishyamramadoss@gmail.com>
2023-11-22 18:52:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Chat Live 
+
+"Chat Live" is a responsive chat platform using React.js for a sleek interface, Node.js for backend functionality, MongoDB for robust data storage, and Socket.io for real-time communication. Styled with CSS's styled components, it offers an intuitive and visually appealing chat experience.
+
+
+## Login
+![login page](./screenshot/login.jpg)
+
+## Register
+![Register page](./screenshot/register.jpg)
+
+## Welcome
+![Welcome page](./screenshot/welcome.jpg)
+
+## Chat
+![Chat page](./screenshot/chat.jpg)
---
+++ public/src/components/Contacts.jsx
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Logout from "./Logout";
+
+export default function Contacts({ contacts, changeChat }) {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+  useEffect(async () => {
+    const data = await JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+    );
+    setCurrentUserName(data.username);
+    setCurrentUserImage(data.avatarImage);
+  }, []);
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelected(index);
+    changeChat(contact);
+  };
+  return (
+    <>
+      {currentUserImage && currentUserImage && (
+        <Container>
+          <div className="brand">
+           
+            <h3>chat live</h3>
+          </div>
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  key={contact._id}
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  onClick={() => changeCurrentChat(index, contact)}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt=""
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <Logout />
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserImage}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="username">
+              <h2>{currentUserName}</h2>
+            </div>
+          </div>
+        </Container>
+      )}
+    </>
+  );
+}
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #F5F5F5;
+  border-top-left-radius: 80px 80px;
+
+
+  .brand {
+    background: linear-gradient(to right,  #a832f7, #f15ac9, #bd148d);
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    height: 4rem;
+    border-bottom: solid;
+    border-color: black;
+    img {
+      height: 1rem;
+    }
+    h3 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  .contacts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    gap: 0.8rem;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .contact {
+      background-color: #b16bf3;
+      min-height: 5rem;
+      cursor: pointer;
+      width: 90%;
+      border-radius: 0.2rem;
+      padding: 0.4rem;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+    .selected {
+      background-color: #9a86f3;
+    }
+  }
+
+  .current-user {
+    background: linear-gradient(to right,  #dcb6f5, #ffa9e8, #fcc3eb);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    border-top: solid;
+    border-color: black;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+`;
